5190919f7ebc18feed88d8ac4160b94a554f756e
Stefan Vercillo <svercillo7@gmail.com>
2021-06-07 03:04:43 -0400

    small script update

--- Backend/scripts/poll_system.py
+++ Backend/scripts/poll_system.py
@@ -10,10 +10,29 @@ current_time = now.strftime("%H:%M:%S")
 
 res = requests.get(f"https://ai-backend-flask.herokuapp.com/api/enumerations?key={os.environ['API_KEY']}")
 
+
+line_count = 0
+try:
+    f = open("logfile.txt", "r")
+
+    for line in f: 
+        if line != "\n":
+            line_count += 1
+    if line_count > 10000:
+        f.close()
+        f = open("logfile.txt", "w+")
+        f.write("NEW FILE\n")
+        f.close()
+except: 
+    pass
+
+f = open("logfile.txt", "a+")    
 if res.status_code != 200:
     print("RUN FAILED")
-    f = open("logfile.txt", "a+")
     f.write(f"Time failed: {current_time}")
-    f.close()
 else:
     print("RUN PASSED")
+    f = open("logfile.txt", "a+")
+    f.write(f"Time failed: {current_time}\n")
+
+f.close()
