069aaa2a6132ce889b67dad4042a9111faa91a8e
Stefan Vercillo <svercillo7@gmail.com>
2021-06-07 02:26:21 -0400

    agnostic test suite

--- Backend/tests/test_server.py
+++ Backend/tests/test_server.py
@@ -1,107 +1,145 @@
 import json 
+import pytest
 import requests
 import os
 from dotenv import load_dotenv
 from os.path import join, dirname
 load_dotenv(join(dirname(__file__), '../.env'))
 
-print("os.environ]")
-print(os.environ["API_KEY"])
-print(os.environ["API_KEY"])
-print(os.environ["API_KEY"])
-print(os.environ["API_KEY"])
-print(os.environ["API_KEY"])
-print(os.environ["API_KEY"])
+LOCAL_URL = "http://localhost:5000"
+PROD_URL = "https://ai-backend-flask.herokuapp.com"
+
 ''' REQUIREMENTS: have server running on localhost:5000 '''
+@pytest.mark.parametrize("environment, authenticated", [
+    ("localhost", True),
+    ("localhost", False),
+    ("production", True),
+    ("production", False)
+])
+def test_home_endpoint(environment, authenticated):
 
-def test_authenticated_request():
+    URL = LOCAL_URL if environment == "localhost" else PROD_URL
+    URL += "/"
 
-    res = requests.get(
-        url=f"http://localhost:5000/?key={os.environ['API_KEY']}", 
-    )
-    assert res.status_code == 200
+    try: 
+        if authenticated:
+            URL += f"?key={os.environ['API_KEY']}"
+            res = requests.get(url=URL)
+            assert res.status_code == 200
+        else:
+            res = requests.get(url=URL)
+            assert res.status_code == 401 
 
-    print("Authenticated request test passed")
-
-
-def test_unauthenticated_request():
-
-    res = requests.get(
-        url="http://localhost:5000" 
-    )
-    assert res.status_code == 401
-
-    print("Unauthenticated request test passed")
-
-
-def test_get_adjacent_nodes():
-    res = requests.post(
-        url=f"http://localhost:5000/api/adjacent_nodes?key={os.environ['API_KEY']}", 
-        data=json.dumps({"lng" :  -79.205745, "lat" : 43.810167 })
-        
-    )
-    print(res.text)
-    assert res.status_code == 200
-    print("Get adjacent nodes test passed")
-
-def test_get_adjacent_nodes_heroku():
-    res = requests.post(
-        url=f"https://ai-backend-flask.herokuapp.com/api/adjacent_nodes?key={os.environ['API_KEY']}", 
-        data=json.dumps({"lng" :  -79.205745, "lat" : 43.810167 })
-        
-    )
-    print(res.text)
-    assert res.status_code == 200
-    print("Get adjacent nodes test passed")
+    except Exception as e:
+        print(f"FAILED RUN: environment {environment}, authenticated {authenticated}, exception: {e}")
 
 
-def test_get_amenities_from_id():
-    res = requests.get(
-        url=f"http://localhost:5000/api/amenities?key={os.environ['API_KEY']}", 
-        params={"id" : "00159f42-8e6d-4c85-ad17-28942a8dd700"}
-    )
-    print(res.text)
-    assert res.status_code == 200
-    print("Get adjacent nodes test passed")
+@pytest.mark.parametrize("environment, authenticated", [
+    ("localhost", True),
+    ("localhost", False),
+    ("production", True),
+    ("production", False)
+])
+def test_home_endpoint(environment, authenticated):
 
+    URL = LOCAL_URL if environment == "localhost" else PROD_URL
+    URL += "/api/amenities"
 
-def test_enumerations_get():
+    try: 
+        if authenticated:
+            URL += f"?key={os.environ['API_KEY']}"
+            res = requests.get(
+                url=URL,
+                params={"id" : "00159f42-8e6d-4c85-ad17-28942a8dd700"}
+            )
+            
+            print(".")
+            print(".")
+            print(res.text)
+            print(".")
+            print(".")
+            assert res.status_code == 200
+        else:
+            res = requests.get(url=URL)
 
-    res = requests.get(
-        url=f"http://localhost:5000/api/enumerations?key={os.environ['API_KEY']}", 
-    )
-    print(res.text)
-    assert res.status_code == 200
+            assert res.status_code == 401
 
-
-    print("Authenticated request test passed")
-
-
-def test_authenticated_request_heroku():
-
-    res = requests.get(
-        url=f"https://ai-backend-flask.herokuapp.com/?key={os.environ['API_KEY']}", 
-    )
-    assert res.status_code == 200
-
-    print("Authenticated request test passed")
+        print(f"PASSED test_home_endpoint: environment {environment}, authenticated {authenticated}")
+    except Exception as e:
+        print(f"TEST test_home_endpoint FAILED RUN: environment {environment}, authenticated {authenticated}, exception: {e}")
 
 
 
-def test_get_unauth_adjacent_nodes():
-    res = requests.post(
-        url="http://ai-backend-flask.herokuapp.com/api/adjacent_nodes?key=12312",
-        data=json.dumps({"lng" :  -79.205745, "lat" : 43.810167 })
-    )
-    print(res.text)
-    assert res.status_code == 401
-    print("Get adjacent nodes test passed")
+@pytest.mark.parametrize("environment, authenticated", [
+    ("localhost", True),
+    ("localhost", False),
+    ("production", True),
+    ("production", False)
+])
+def test_enumerations_endpoint(environment, authenticated):
 
-# def test_get_unauth_adjacent_nodes():
-#     res = requests.post(
-#         url="http://localhost:5000/api/adjacent_nodes?key=12312",
-#         data=json.dumps({"lng" :  -79.205745, "lat" : 43.810167 })
-#     )
-#     print(res.text)
-#     assert res.status_code == 200
-#     print("Get adjacent nodes test passed")
+    URL = LOCAL_URL if environment == "localhost" else PROD_URL
+    URL += "/api/enumerations"
+
+    try: 
+        if authenticated:
+            URL += f"?key={os.environ['API_KEY']}"
+            res = requests.get(url=URL)
+            assert res.status_code == 200
+            print(".")
+            print(".")
+            print(res.text)
+            print(".")
+            print(".")
+            data = json.loads(res.data)
+            assert isinstance(data, list)
+            assert len(data) > 0
+        else:
+            res = requests.get(url=URL)
+            assert res.status_code == 401
+
+        print(f"PASSED test_home_endpoint: environment {environment}, authenticated {authenticated}")
+    except Exception as e:
+        print(f"TEST test_home_endpoint FAILED RUN: environment {environment}, authenticated {authenticated}, exception: {e}")
+
+
+@pytest.mark.parametrize("environment, authenticated", [
+    ("localhost", True),
+    ("localhost", False),
+    ("production", True),
+    ("production", False)
+])
+def test_adjacent_nodes_endpoint(environment, authenticated):
+
+    URL = LOCAL_URL if environment == "localhost" else PROD_URL
+    URL += "/api/adjacent_nodes"
+
+    try: 
+        if authenticated:
+            URL += f"?key={os.environ['API_KEY']}"
+            res = requests.post(
+                url=URL, 
+                data=json.dumps({"lng" :  -79.205745, "lat" : 43.810167 })
+                
+            )
+            assert res.status_code == 200
+            print(".")
+            print(".")
+            print(res.text)
+            print(".")
+            print(".")
+            data = json.loads(res.data)
+            assert isinstance(data, list)
+            assert len(data) > 0
+        else:
+            res = requests.post(
+                url=URL, 
+                data=json.dumps({"lng" :  -79.205745, "lat" : 43.810167 })
+                
+            )
+            assert res.status_code == 401
+
+        print(f"PASSED test_home_endpoint: environment {environment}, authenticated {authenticated}")
+    except Exception as e:
+        print(f"TEST test_home_endpoint FAILED RUN: environment {environment}, authenticated {authenticated}, exception: {e}")
+
